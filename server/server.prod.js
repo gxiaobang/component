7f07d5221635f7899f67f0f75a3d9f9101eb79e9
gxiaobang <gxiaobang@163.com>
2017-03-05 10:29:57 +0800

    配置prod代理服务器

--- server/server.prod.js
+++ server/server.prod.js
@@ -1,34 +1,48 @@
 /**
- * webpack热启动
+ * 生产环境node代理
  */
 
-var webpack = require('webpack'),
-		WebpackDevServer = require('webpack-dev-server'),
-		opn = require('opn');
+const express = require('express');
+const proxy = require('http-proxy-middleware');
+const opn = require('opn');
 
-var config = require('./webpack.config');
-var compiler = webpack(config);
+const app = express();
 
-const port = 8000;
+const path = require('path');
 
-var server = new WebpackDevServer(compiler, {
-	// publicPath: config.output.publicPath,
-	hot: true,
-	historyApiFallback: true,
-	inline: true,
-	progress: true,
-	stats: {
-		color: true,
-		hash: false,
-		timings: true,
-		chunks: false,
-		chunkModules: false,
-		modules: false
+// 参数获取
+const port = '8080';
+const host = '0.0.0.0';
+
+// 真实服务器
+app.use('/api', proxy({
+	target: 'xxx', // 代理服务的地址,
+	changeOrigin: true,
+	pathRewrite: {
+		'^/api': ''
 	}
+}));
+
+// mock服务器
+app.use('/mock', proxy({
+	target: 'xxx', // 代理服务的地址,
+	changeOrigin: true,
+	pathRewrite: {
+		'^/mock': ''
+	}
+}));
+
+// 静态文件
+app.use(express.static(path.resolve('./build')));
+
+// 重定向到主页
+app.get('*', (req, res) => {
+	res.sendFile('xxx');
 });
 
+
 // 监听3000端口
-server.listen(port, 'localhost', (err) => {
+app.listen(port, host, (err) => {
 	if (err) throw err;
 	console.log(`Listening at http:localhost:${port}`);
 	opn(`http://localhost:${port}`);
