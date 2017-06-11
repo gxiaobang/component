8e59a6905b59d43788332dcd41903383272f7be9
gxiaobang <gxiaobang@163.com>
2017-06-11 14:23:40 +0800

    加载动画

--- src/components/Spin/index.jsx
+++ src/components/Spin/index.jsx
@@ -13,8 +13,13 @@ class Spin extends React.Component {
   render() {
     return (
       <div className="spin-wrapper">
-        <span className="spin">
-          <Icon type="spinner" size={16} />
+        <span className="spin-anim">
+          {/*<Icon type="spinner" size={16} />*/}
+          <span className="spin-circle"></span>
+          <span className="spin-circle"></span>
+          <span className="spin-circle"></span>
+          <span className="spin-circle"></span>
+          <span className="spin-circle"></span>
         </span>
         <span>{this.props.children || '加载中...'}</span>
       </div>
